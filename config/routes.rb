87516a2c6967ec1b14feb9c1bf690d1cd9452a7b
yoong9627 <yoong9627@gmail.com>
2018-04-06 09:22:58 -0700

    init

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
+
+  get 'main/home'
+  root "main#home"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+end
